6d5046acbadc66a9406cc3c704fbaeddb5083480
Jestape2 <damirdus2012@gmail.com>
2026-06-20 18:11:50 +0500

    String

--- # Задание 1.py
+++ # Задание 1.py
@@ -1,15 +1,9 @@
 email = input("Введите электронную почту: ")
-
-# 1. Находим позицию символа @ в строке
 at_position = email.find("@")
 
 if at_position != -1:
-    # 2. Находим длину строки
     email_length = len(email)
-    
-    # 3. Вырежем подстроку от символа @ до конца строки
     domain = email[at_position + 1 : email_length]
-    
     print("Домен почты:", domain)
 else:
     print("Ошибка: в тексте нет символа @")
