72c74d58ce41107266d8bd2bf9349647e6049961
Jestape2 <damirdus2012@gmail.com>
2026-06-20 18:44:30 +0500

    КМН (Проектная Работа).py

--- КМН (Проектная Работа).py
+++ КМН (Проектная Работа).py
@@ -7,26 +7,22 @@ def get_computer_choice():
 def determine_winner(vvod_igroka, vvod_kompa):
     if vvod_igroka == vvod_kompa: 
         return "nichya"
-    
-    if (vvod_igroka == "камень" and vvod_kompa == "ножницы") or \
+     if (vvod_igroka == "камень" and vvod_kompa == "ножницы") or \
        (vvod_igroka == "ножницы" and vvod_kompa == "бумага") or \
        (vvod_igroka == "бумага" and vvod_kompa == "камень"):
         return "igrok"
-    
     return "komputer"
 
 def main():
     print("Игра до 3 очков.")
     ochki_igroka = 0
     ochki_kompa = 0
-    
     while ochki_igroka < 3 and ochki_kompa < 3:
         vvod_polzovatelya = input("\nВаш ход: ").lower().strip()
         peremennaya_kompa = get_computer_choice()
         print(f"Компьютер: {peremennaya_kompa}")
         
-        itog_raunda = determine_winner(vvod_polzovatelya, peremennaya_kompa)
-        
+    itog_raunda = determine_winner(vvod_polzovatelya, peremennaya_kompa)
         if itog_raunda == "igrok": 
             ochki_igroka += 1
         elif itog_raunda == "komputer": 
@@ -42,3 +38,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Может быть небольшое смещение кода. Прошу прощения(
